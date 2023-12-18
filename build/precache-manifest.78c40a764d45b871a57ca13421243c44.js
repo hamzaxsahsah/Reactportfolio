10f8a8ef8788cc9bef8a95b7c1f95b805970ee59
Hamza Sahsah <nanobig2000@gmail.com>
2023-12-18 21:41:38 +0100

    commiting build

---
+++ build/precache-manifest.78c40a764d45b871a57ca13421243c44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74da5f1dfbffa47b8ce4023bdb274765",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7f206c7b89ec5ae7bb9",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "11f836e08193c53e8131",
+    "url": "/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "b7f206c7b89ec5ae7bb9",
+    "url": "/static/js/2.60a93a86.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.60a93a86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11f836e08193c53e8131",
+    "url": "/static/js/main.855ceab4.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
